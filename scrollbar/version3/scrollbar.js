46363a31223d3a73d53439181e7384115fe0904d
Tim Greller <timlg07@gmail.com>
2020-08-16 22:56:09 +0200

    Scrollbar [v3]: removed unnecessary event listener.

--- scrollbar/version3/scrollbar.js
+++ scrollbar/version3/scrollbar.js
@@ -1,11 +1,10 @@
-window.addEventListener('load', () => {
-    window.addEventListener('mousemove', evt => {
-        const detectionRange = 5/*px*/;
-        const hoverClass = 'scrollbar-hover';
-        if (evt.clientX >= (document.body.clientWidth - detectionRange)) {
-            document.body.classList.add(hoverClass);
-        } else {
-            document.body.classList.remove(hoverClass);
-        }
-    });
+window.addEventListener('mousemove', evt => {
+    const detectionRange = 5/*px*/;
+    const hoverClass = 'scrollbar-hover';
+    
+    if (evt.clientX >= (document.body.clientWidth - detectionRange)) {
+        document.body.classList.add(hoverClass);
+    } else {
+        document.body.classList.remove(hoverClass);
+    }
 });
